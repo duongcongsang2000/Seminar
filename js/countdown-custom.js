65f94340ffcf7272358016dfc4c658f69d708d5b
Sang D <duongcongsang2000@gmail.com>
2024-10-01 10:28:16 +0700

    update 01102024

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2024, 7, 8, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2024, 10, 1, 8)}); // year, month, date, hour
         });
 });		
 
